16bebde0239d206e7b3d49e93f2da0c3e0fe6034
Nicusor Iosef <gogu69@yahoo.com>
2024-08-05 17:37:21 +0300

    Photo gallery - functional

--- photo.php
+++ photo.php
@@ -3,57 +3,29 @@
  <html>
  <head>
  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
- <title>Title</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Simple Photo Gallery</title>
  <meta name="language" content="en" /> 
-
- <meta name="description" content="" /> 
-
- <meta name="keywords" content="" />
  <link rel="preconnect" href="https://fonts.googleapis.com">
  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
  <link href="https://fonts.googleapis.com/css2?family=Kadwa:wght@400;700&display=swap" rel="stylesheet">
-   
- <style type="text/css">
-   
-h1 {font-family: "Kadwa", serif; font-weight: 700; font-style: normal;font-size: 32px; text-align: center;}
-p {font-family: "Kadwa", serif; font-weight: 400; font-style: normal;font-size: 18px; text-align: center; color: red;}
-img {width: 400px; height: 250px; object-fit: cover; background-color: aqua;}
-img:hover {filter:drop-shadow(0px 0px 15px rgba(0, 0, 0, 0.4))}
-.container {display: flex; flex-direction:column; justify-content: center; align-items: center;}
-/* 
-.main {display: flex; height: 100vh; justify-content: center; align-items: center; flex-direction: column;}
-.container {display: flex; flex-direction:column; justify-content: center; width:80%; height: 80%;}
-*/
-
-
-   @media (min-width: 850px) 
-   {
-	.main {display: flex; height: 100vh; justify-content: center; align-items: center; flex-direction: column;}
-	.container {display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center; align-content: flex-start; width:80%; height: 80%;}
-	img {width: 250px; height: 180px; object-fit: cover; background-color: aqua;}
-	img:hover {filter:drop-shadow(0px 0px 15px rgba(0, 0, 0, 0.4))}
-	h1 {font-family: "Kadwa", serif; font-weight: 700; font-style: normal;font-size: 32px; text-align: center;}
-	p {font-family: "Kadwa", serif; font-weight: 400; font-style: normal;font-size: 18px; text-align: center; color: green;}   
-  } 
-
-
- </style>
-   
+ <link rel="stylesheet" href="photo.css">
+  
  </head>
  <body>
-	<div class="main">
-    	<div>
-        	<h1>Album foto</h1>
-          	<p> Click pe imagine => descarca imaginea</p>
-     	</div>
+  <div class="main">
+    <div class="description">
+        <h1>Simple Photo Album</h1>
+        <p>You can choose the picture to download by clicking on it.</p>
+    </div>
     
-    	<div class="container">
-
+    <div class="container">
+            
             <!-- PICTURE GALLERY -->
             <?php
 
             // open this directory 
-            $myDirectory = opendir("igrd/images/large");
+            $myDirectory = opendir("images");
 
             // get each entry
             while($entryName = readdir($myDirectory)) {
@@ -73,14 +45,14 @@ img:hover {filter:drop-shadow(0px 0px 15px rgba(0, 0, 0, 0.4))}
             for($index=0; $index < $indexCount; $index++) {
             $extension = substr($dirArray[$index], -3);
             if ($extension == 'jpg'){ // list only jpgs
-            //echo '<div class="picture"><img src="igrd/images/large/' . $dirArray[$index] . '" alt="Image" /></div>';
-             echo '<div class="picture"><a href="igrd/images/large/' . $dirArray[$index] . '" download> <img src="igrd/images/large/' . $dirArray[$index] . '" alt="Image" /> </a></div>';
+            echo '<div class="picture"><a href="images/' . $dirArray[$index] . '" download> <img src="images/' . $dirArray[$index] . '" alt="Image" /> </a></div>';
             } 
             }
             ?>
         
-   		</div>
-	</div>
+   	</div>
+     <p>Simple photo album, helps to transfer images. Displays all images from a folder that can be downloaded by simply clicking on them.</p>
+  </div>
 
   </body>
  </html>
